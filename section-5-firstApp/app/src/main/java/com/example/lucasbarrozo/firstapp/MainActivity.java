16d22588b0e36624afaf5a106e570562a619369c
Lucas Lange Barrozo <lucaslbarrozo@gmail.com>
2018-08-12 19:28:06 -0300

    cria botao que altera texto

--- section-5-firstApp/app/src/main/java/com/example/lucasbarrozo/firstapp/MainActivity.java
+++ section-5-firstApp/app/src/main/java/com/example/lucasbarrozo/firstapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.lucasbarrozo.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void alterarTexto (View view) {
+
+        TextView text = findViewById(R.id.exibitionText);
+        text.setText("TEXTO ALTERADO");
     }
 }
