608851610e5bb0e70cfcaa4b68ff81e0fe147e31
Lucas Lange Barrozo <lucaslbarrozo@gmail.com>
2018-08-14 13:21:38 -0300

    faz lógica para aparecer aleatoriamente frases de um array

--- section-6-frasesDoDia/app/src/main/java/com/example/lucasbarrozo/frasesdodia/MainActivity.java
+++ section-6-frasesDoDia/app/src/main/java/com/example/lucasbarrozo/frasesdodia/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.lucasbarrozo.frasesdodia;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void gerarFrase (View view){
+
+        TextView texto = findViewById(R.id.text_resultado);
+
+        String [] frases = {"Frase 1", "Frase 2", "Frase 3"};
+
+        int numero = new Random().nextInt(frases.length);
+
+        texto.setText(frases[numero]);
+
+
 
     }
 }
