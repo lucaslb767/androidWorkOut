83a8bbb112b198ea57c032aa20e9d1d03eb9e8b6
Lucas Lange Barrozo <lucaslbarrozo@gmail.com>
2018-08-16 22:04:10 -0300

    faz os gates logicos para determinar o vencedor

--- section-7-pedraPapelTesoura/app/src/main/java/com/example/lucasbarrozo/myapplication/MainActivity.java
+++ section-7-pedraPapelTesoura/app/src/main/java/com/example/lucasbarrozo/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -29,7 +30,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void opcaoSelecionada(String escolhaUsuario){
 
-        ImageView imgResultado = findViewById(R.id.imgResultado);
+        ImageView imgResultado = (ImageView) findViewById(R.id.imgResultado);
+
+        TextView txtResultado = findViewById(R.id.txtResultado);
 
         //gerar número aleatorio para escolha do App
 
@@ -52,6 +55,27 @@ public class MainActivity extends AppCompatActivity {
             case "tesoura":
                 imgResultado.setImageResource(R.drawable.tesoura);
                 break;
+        }
+
+
+        if ((escolhaApp == "pedra" && escolhaUsuario=="tesoura") || (escolhaApp == "papel" && escolhaUsuario=="pedra") || (escolhaApp == "tesoura" && escolhaUsuario=="papel")){
+            //app ganha
+
+            txtResultado.setTextColor(this.getColor(R.color.red));
+            txtResultado.setText("Você Perdeu");
+
+        } else if ((escolhaApp == "pedra" && escolhaUsuario=="papel") || (escolhaApp == "papel" && escolhaUsuario=="tesoura") || (escolhaApp == "tesoura" && escolhaUsuario=="pedra")){
+
+            //usuário ganha
+
+            txtResultado.setTextColor(this.getColor(R.color.green));
+            txtResultado.setText("Vitória!");
+        } else {
+
+            //empate
+
+            txtResultado.setTextColor(this.getColor(R.color.yellow));
+            txtResultado.setText("Empate!");
         }
 
 
